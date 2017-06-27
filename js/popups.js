f6a9c21edcc8727b2e9800dcae09da4815d606b6
Siarhei on work <s.chaptsau@gmail.com>
2017-06-27 14:17:33 +0300

    popups

--- js/popups.js
+++ js/popups.js
@@ -1,35 +1,35 @@
-var popupFailure = document.querySelector(".popup--failure");
-var popupSuccess = document.querySelector(".popup--success");
-var formButton = document.querySelector(".field-action__btn");
-var popupButton = document.querySelectorAll(".popup__btn");
+var Failure = document.querySelector(".popup--failure");
+var Success = document.querySelector(".popup--success");
+var Btn = document.querySelector(".field-action__btn");
+var PopupBtn = document.querySelectorAll(".popup__btn");
 
-formButton.addEventListener("click", function(event) {
+Btn.addEventListener("click", function(event) {
   event.preventDefault();
   if(!document.getElementsByName("name")[0].value
     && !document.getElementsByName("surname")[0].value
     && !document.getElementsByName("email")[0].value) {
-      popupFailure.classList.add("popup--show");
+      Failure.classList.add("popup--show");
   } else {
-    popupSuccess.classList.add("popup--show");
+    Success.classList.add("popup--show");
   }
 });
 
-for (var i = 0; i < popupButton.length; i++) {
-  popupButton[i].addEventListener("click", function(event) {
-    if (popupSuccess.classList.contains("popup--show")) {
-      popupSuccess.classList.remove("popup--show");
-    } else if (popupFailure.classList.contains("popup--show")) {
-      popupFailure.classList.remove("popup--show");
+for (var i = 0; i < PopupBtn.length; i++) {
+  PopupBtn[i].addEventListener("click", function(event) {
+    if (Success.classList.contains("popup--show")) {
+      Success.classList.remove("popup--show");
+    } else if (Failure.classList.contains("popup--show")) {
+      Failure.classList.remove("popup--show");
     }
   });
 }
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
-    if (popupSuccess.classList.contains("popup--show")) {
-      popupSuccess.classList.remove("popup--show");
-    } else if (popupFailure.classList.contains("popup--show")) {
-      popupFailure.classList.remove("popup--show");
+    if (Success.classList.contains("popup--show")) {
+      Success.classList.remove("popup--show");
+    } else if (Failure.classList.contains("popup--show")) {
+      Failure.classList.remove("popup--show");
     }
   }
 });
